9c6fec2fe75e4fac8e6ec4ab3212fa5537289dae
Fernando Avellar Jr. <favellar@yahoo.com.br>
2024-07-05 13:49:47 -0300

    After remove coin and create settings services

---
+++ src/main/java/com/avellar/userapi/controller/SettingsController.java
@@ -0,0 +1,47 @@
+package com.avellar.userapi.controller;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.avellar.userapi.dto.CurrencyRequest;
+import com.avellar.userapi.services.SettingsService;
+
+@RestController
+@RequestMapping("/settings")
+public class SettingsController {
+
+	@Autowired
+	private SettingsService settingsService;
+
+	@GetMapping
+	public ResponseEntity<Set<String>> getAllowedCurrencies() {
+		return ResponseEntity.ok(settingsService.getAllowedCurrencies());
+	}
+	
+	@PostMapping
+    public ResponseEntity<Void> addCurrency(@RequestBody CurrencyRequest request) {
+        if (settingsService.addCurrency(request.currency())) {
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @DeleteMapping("/{currency}")
+    public ResponseEntity<Void> removeCurrency(@PathVariable String currency) {
+        if (settingsService.removeCurrency(currency)) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+}
